c64f2ba7a389f7ec4a0751303e8defdb5d821039
Priyabrata Nandy <pritsnan@gmail.com>
2020-06-24 11:14:38 +0530

    Addition prog and modified first prog

---
+++ SecondProg.java
@@ -0,0 +1,12 @@
+
+public class SecondProg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int first = 4;
+		int second = 8;
+		System.out.println(first + second);
+
+	}
+
+}
